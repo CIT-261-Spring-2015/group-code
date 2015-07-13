a09906367497deab7ff2bf275476a061ed30acb1
Bdawgrock <bdawgrock3000@gmail.com>
2015-07-13 14:59:17 -0600

    Legit Card Transitions End of Class
    Flip animation from deck to hand to discard.

--- cardFlipTest/public_html/js/flipTrigger.js
+++ cardFlipTest/public_html/js/flipTrigger.js
@@ -1,4 +1,25 @@
+
+window.onload = addClickDraw;
+function addClickDraw(){
+      var c = document.getElementById("flipContainer");
+      c.addEventListener("click", flip);
+}
+
 function flip(){
     var c = document.getElementById("flipContainer");
     c.classList.add("flipped");
+    addClickDiscard();
+}
+
+
+function addClickDiscard(){
+       var c = document.getElementById("flipContainer");
+       c.addEventListener("click", discard);
+}
+
+function discard(){
+    var c = document.getElementById("flipContainer");
+    c.classList.add("discard");
+    console.log("It worked");
+     
 }
