b0cc20ee9840fca409f18c1675c2ce12edb2fb80
Bdawgrock <bdawgrock3000@gmail.com>
2015-07-13 14:10:03 -0600

    Revert "Better Code"
    This reverts commit 9bdfccccd4a212d9a4c02b10b9849a7372c4e1a2.

--- cardFlipTest/public_html/js/flipTrigger.js
+++ cardFlipTest/public_html/js/flipTrigger.js
@@ -1,20 +1,4 @@
-window.onload = addClickDraw;
-//Add click event listener for the drawing of a card animation function
-function addClickDraw() {
-    var c = document.getElementById("flipContainer");
-    c.addEventListener("click", flip);
-}
-
-//Add click event listener for the discarding of a card animation function
-//function addClickDiscard() {
-//    var d = document.getElementById("flipContainer");
-//    
-//    
-//    d.addEventListener("click", flip);
-//}
-
-//Add the css animation class
-function flip() {
+function flip(){
     var c = document.getElementById("flipContainer");
     c.classList.add("flipped");
 }
